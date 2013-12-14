c404910d21e10e718ff4cc5c939e5dd481208618
Vicente Málaga <vmalaga@gmail.com>
2013-12-14 22:16:02 +0100

    http redirect to passdb

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from passManager import views
 from passManager.views import sendPassEmailView, mailsent
+from django.http import HttpResponseRedirect
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -10,6 +11,6 @@ urlpatterns = patterns('',
                        url(r'^send_pass/(?P<rowid>\d+)/$', sendPassEmailView),
                        url(r'^send_pass/(send)/$', sendPassEmailView),
                        url(r'^mailsent/$', mailsent),
+		       url(r'^$', lambda x: HttpResponseRedirect('/admin/passManager/passdb/')),
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', include(admin.site.urls)),
 )
